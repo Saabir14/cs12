bf461212fba28fc20fc6c987a97b12c52d5d7dde
Saabir14 <64490729+Saabir14@users.noreply.github.com>
2021-11-25 09:05:11 +0400

    Finish

--- pets.py
+++ pets.py
@@ -6,9 +6,18 @@ def get_char(prompt=''):
 array = [[(input('Owner Name: ')), input('Pet Type: ')]]
 print(array)
 
-x = get_char('1.\tAdd another owner and pet\n2.\tChange/Amend an owner/pet\n3.\tAdd new owner and pet\n4.\tPrint the list of owners and pets\n\nChoose: ')
-if x == 1: array.append([(input('Owner Name: ')), input('Pet Type: ')])
-elif x == 2:
-    array[input('Index: ')] = [(input('Owner Name: ')), input('Pet Type: ')]
-elif x == 3: array.append([(input('Owner Name: ')), input('Pet Type: ')])
-elif x == 4: print(array)
+while True:
+    x = get_char('1.\tAdd another owner and pet\n2.\tChange/Amend an owner/pet\n3.\tAdd new owner and pet\n4.\tPrint the list of owners and pets\n5.\tBreak\n\nChoose: ')
+    if x == 1: array.append([(input('Owner Name: ')), input('Pet Type: ')])
+    elif x == 2:
+        array[input('Index: ')] = [(input('Owner Name: ')), input('Pet Type: ')]
+    elif x == 3: array.append([(input('Owner Name: ')), input('Pet Type: ')])
+    elif x == 4: print(array)
+    else: break
+
+with open('Customer.txt', 'w') as file:
+    for i in array:
+        file.write(i[0])
+        file.write(', ')
+        file.write(i[1])
+        file.write('\n')
