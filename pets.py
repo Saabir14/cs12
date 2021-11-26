cd42b14b3aec7a41790797d17ef1d1b7955630dd
Saabir14 <64490729+Saabir14@users.noreply.github.com>
2021-11-26 07:16:54 +0400

    Code Done

--- pets.py
+++ pets.py
@@ -1,20 +1,24 @@
-def get_char(prompt=''):
-    x = input(prompt)
-    if len(x) != 1: return get_char(prompt)
-    else: return x
+def get_int(prompt):
+    try: return(int(input(prompt)))
+    except: return(get_int(prompt))
+
+def get_num(prompt, max=0, min=0):
+    x = get_int(prompt)
+    if x <= max and x >= min: return x
+    else: return get_num(prompt, max, min)
 
 array = [[(input('Owner Name: ')), input('Pet Type: ')]]
-print(array)
 
 while True:
-    x = get_char('1.\tAdd another owner and pet\n2.\tChange/Amend an owner/pet\n3.\tAdd new owner and pet\n4.\tPrint the list of owners and pets\n5.\tBreak\n\nChoose: ')
+    x = get_int('1.\tAdd another owner and pet\n2.\tChange/Amend an owner/pet\n3.\tAdd new owner and pet\n4.\tPrint the list of owners and pets\n5.\tBreak and save to file\n\nChoose: ')
     if x == 1: array.append([(input('Owner Name: ')), input('Pet Type: ')])
     elif x == 2:
-        array[input('Index: ')] = [(input('Owner Name: ')), input('Pet Type: ')]
+        y = get_num('Index: ', min=0, max=len(array) - 1)
+        array[y] = [(input('Owner Name: ')), input('Pet Type: ')]
     elif x == 3: array.append([(input('Owner Name: ')), input('Pet Type: ')])
     elif x == 4: print(array)
     else: break
 
 with open('Customer.txt', 'w') as file:
     for i in array:
-        file.writelines(i[0] + ', ' + i[1])
+        file.write(i[0] + ', ' + i[1] + '\n')
