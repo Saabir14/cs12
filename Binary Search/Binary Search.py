cff51ba721253e90cd2547915568f81cbd37e5b2
Saabir14 <64490729+Saabir14@users.noreply.github.com>
2021-12-26 12:29:54 +0400

    made binary searche more effecient

--- Binary Search/Binary Search.py
+++ Binary Search/Binary Search.py
@@ -6,7 +6,7 @@ def get_int(dialog):
 
 def split(a, n):                                    # Function to split an 'a' into 'n' parts that are as even as possible and return the split parts
     k, m = divmod(len(a), n)
-    return (a[i*k+min(i, m):(i+1)*k+min(i+1, m)] for i in range(n))
+    return list((a[i*k+min(i, m):(i+1)*k+min(i+1, m)] for i in range(n)))
 
 def index(array):
     tmp = []
@@ -24,13 +24,20 @@ def binarysearch(array, target):
         if array[0][1] == target: return array[0][0]
         else: return -1
 
-    for i in split(array, 2):
-        if binarysearch(i, target) != -1: return binarysearch(i, target)
+    slicedarray = split(array, 2)
+    if slicedarray[1][0][1] > target:
+        a = binarysearch(slicedarray[0], target)
+        if a != -1: return a
+    else:
+        a = binarysearch(slicedarray[-1], target)
+        if a != -1: return a
 
     return -1
 
 
-x = binarysearch(range(get_int('Array size: ')), get_int('Number to searche: '))
+array = range(get_int('Array Size: '))
+x = binarysearch(array, get_int('Number to searche: '))
+
 if x == -1:
     print('Target not Found')
 else:
